4e1ad009ff066450b32d927926a38c6ae5f5dcb2
mauryasid1999 <54549509+mauryasid1999@users.noreply.github.com>
2020-07-21 12:28:04 +0530

    Update settings.py

--- intern/settings.py
+++ intern/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='test@gmail.com'
-EMAIL_HOST_PASSWORD='*************'
+#EMAIL_HOST_PASSWORD='*************'
 EMAIL_PORT='587'
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
